f72f79ab15b00f8efc5cb3b6209441750844c885
louis <tl721@hotmail.com>
2023-02-25 11:36:34 -0400

    Deleted footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,6 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      // href='https://github.com/rajshekhar26/cleanfolio'
       className='link footer__link'
     >
       Created By Louis Tang
